9895798dc1aa5b59a38366a8fbcb90c579f751e0
ZumDeWald <zumdewald@gmail.com>
2020-07-07 10:29:53 -0500

    Added drop zone

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Shrink Ray</h1>
       </header>
+      <main>
+        <section class="drop-zone-container">
+          <div id="drop-zone" ondrop="handleDrop(e)">
+            <h3>Drop Zone</h3>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
