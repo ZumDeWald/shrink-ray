886682b7e4c31b340cd1a0426a13552572d01096
Eric Zumwalt <ezumwalt@cs1303.jmm.org>
2020-07-08 15:05:24 -0500

    Check file type on drop

--- src/components/FileList.js
+++ src/components/FileList.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import DragAndDrop from "./DragAndDrop.js";
 
 const FileList = ({ props }) => {
+  const [formats, setFormats] = useState(["jpg", "jpeg", "png"]);
+
   const [files, setFiles] = useState([
     "nice.png",
     "veryCool.jpg",
@@ -13,10 +15,31 @@ const FileList = ({ props }) => {
   ]);
 
   const handleDropProp = passedFiles => {
+    //Existing file list
     let fileList = JSON.parse(JSON.stringify(files));
-    for (let i = 0; i < passedFiles.length; i++) {
-      if (!passedFiles[i].name) return;
-      fileList.push(passedFiles[i].name);
+
+    //Change passedFiles into array
+    let newFiles = [...passedFiles];
+
+    //Check if acceptable file type in formats array
+    if (
+      formats &&
+      newFiles.some(
+        file =>
+          !formats.some(format =>
+            file.name.toLowerCase().endsWith(format.toLowerCase())
+          )
+      )
+    ) {
+      alert(
+        `Only following file formats are acceptable: ${formats.join(", ")}`
+      );
+      return;
+    }
+
+    for (let i = 0; i < newFiles.length; i++) {
+      if (!newFiles[i].name) return;
+      fileList.push(newFiles[i].name);
     }
     setFiles(fileList);
   };
