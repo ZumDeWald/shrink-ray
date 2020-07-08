e7eb662169c4a04971037729fa5698da5ec870e0
Eric Zumwalt <ezumwalt@cs1303.jmm.org>
2020-07-08 16:02:08 -0500

    Began troubleshooting the Magick

--- src/App.js
+++ src/App.js
@@ -19,6 +19,11 @@ function App() {
         <section className="drop-zone-container">
           <FileList />
         </section>
+        <img
+          id="test-image"
+          src="https://images.unsplash.com/photo-1593935532255-a0574cc8fbf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=789&q=80"
+          alt="Trying it"
+        />
       </main>
     </div>
   );
--- src/components/FileList.js
+++ src/components/FileList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import DragAndDrop from "./DragAndDrop.js";
+import { execute, buildInputFile, loadImageElement } from "wasm-imagemagick";
 
 const FileList = ({ props }) => {
   const [formats, setFormats] = useState(["jpg", "jpeg", "png"]);
@@ -13,6 +14,26 @@ const FileList = ({ props }) => {
     "allTheCats.jpg",
     "catsWithBats.jpeg",
   ]);
+
+  async function magick(file) {
+    const { outputFiles, exitCode, stderr } = await execute({
+      inputFiles: [
+        await buildInputFile(URL.createObjectURL(file), "image1.png"),
+      ],
+      commands: `
+        convert image1.png -bordercolor #ffee44 -background #eeff55 +polaroid image2.png
+        convert image2.png -fill #997711 -tint 55 image3.jpg
+      `,
+    });
+    if (exitCode) {
+      alert(`There was an error with the command: ${stderr.join("\n")}`);
+    } else {
+      await loadImageElement(
+        outputFiles.find(f => f.name === "image3.jpg"),
+        document.getElementById("test-image")
+      );
+    }
+  }
 
   const handleDropProp = passedFiles => {
     //Existing file list
@@ -41,7 +62,16 @@ const FileList = ({ props }) => {
       if (!newFiles[i].name) return;
       fileList.push(newFiles[i].name);
     }
+
     setFiles(fileList);
+
+    //Display first image
+    // if (!!newFiles.length) {
+    //   document.querySelector("#test-image").src = URL.createObjectURL(newFiles[0])
+    // };
+
+    //Trying the magick
+    magick(newFiles[0]);
   };
 
   return (
