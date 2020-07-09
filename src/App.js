76db573ab5ff717964a924472fe2b0b491ef0eee
Eric Zumwalt <ezumwalt@cs1303.jmm.org>
2020-07-09 15:28:14 -0500

    Can download zips of processed files

--- src/App.js
+++ src/App.js
@@ -11,29 +11,30 @@ function App() {
         <h1>Shrink Ray</h1>
       </header>
       <main>
-        {
-          // <section className="drop-zone-container">
-          //   <div id="drop-zone" onDrop={() => {}}>
-          //     <h3>Drop Zone</h3>
-          //   </div>
-          // </section>
-        }
         <section className="drop-zone-container">
           <FileList setImageProcessed={setImageProcessed} />
         </section>
-        <section className="output-image-container">
+
+        <section
+          className={
+            imageProcessed
+              ? "output-image-container"
+              : "output-image-container ghost"
+          }
+        >
           <img
             id="output-image"
             src="https://images.unsplash.com/photo-1593935532255-a0574cc8fbf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=789&q=80"
             alt="Trying it"
           />
-          {!!imageProcessed && (
-            <div>
-              <a id="download-link" href="/" download="result.zip">
-                <button className="download-button">Download Results</button>
-              </a>
-            </div>
-          )}
+
+          <div>
+            <a id="download-link" href="/" download="result.zip">
+              <button className="download-button">
+                Download Results From Last Upload
+              </button>
+            </a>
+          </div>
         </section>
       </main>
     </div>
