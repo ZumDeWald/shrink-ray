d84fac9651fdba3f0583ea792e3cf8c814c87405
Eric Zumwalt <ezumwalt@CS1303.attlocal.net>
2020-07-17 13:03:39 -0500

    The latest

--- src/components/Magick.js
+++ src/components/Magick.js
@@ -22,11 +22,9 @@ async function Magick(file, commandOptionParams) {
 
   commandOptionParams.forEach((param, i) => {
     commandOptions.push(
-      `
-    convert inputImage.${fileType} -resize ${param.size} image${i}.${fileType}`,
-      `image${i}.${fileType} -quality ${param.quality} -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 image${i}2.${fileType}
-    image${i}2.${fileType} -strip final_v${i}.${fileType}
-    `
+      `convert inputImage.${fileType} -resize ${param.size} image${i}.${fileType}`,
+      `convert image${i}.${fileType} -quality ${param.quality} -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 image${i}2.${fileType}`,
+      `convert image${i}2.${fileType} -strip final_v${i}.${fileType}`
     );
   });
 
